5d137ef0640f84ec4c1ef63b457deda737e24be4
Xiaxle <yusuf.qamail@gmail.com>
2023-04-01 22:43:30 +0300

    asd

--- src/test/java/runners/GoogleRunner.java
+++ src/test/java/runners/GoogleRunner.java
@@ -1,14 +1,16 @@
 package runners;
 
-import io.cucumber.testng.AbstractTestNGCucumberTests;
-import io.cucumber.testng.CucumberOptions;
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
 
+@RunWith(Cucumber.class)
 @CucumberOptions(
-        features = {"src/test/resources/features/Google.feature"},
+        features = {"src/test/resources/features"},
         glue = {"stepDefinitions"},
         plugin = {"pretty", "html:target/cucumber-reports.html", "json:target/json-reports/cucumber.json", "junit:target/xml-report/cucumber.xml"},
         dryRun = false,
         tags = "@Google"
 )
-public class GoogleRunner extends AbstractTestNGCucumberTests {
+public class GoogleRunner {
 }
